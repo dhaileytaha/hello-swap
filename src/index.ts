0745781049447da5840a0ca0187b798738ec7950
Franck Royer <franck@coblox.tech>
2019-09-02 13:50:39 +1000

    Document some assumptions

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-/// Orchestrate the hello swap apps
-
-// Start 2 instances of helloSwap
-// "Link" them together. eg extract PeerId etc.
-// Call "sendSwap()" on one instance which is going to trigger a swap query
+/** Orchestrate the hello swap apps
+ * Start 2 instances of helloSwap
+ * "Link" them together. eg extract PeerId etc.
+ * Call "sendSwap()" on one instance which is going to trigger a swap query
+ * Assumptions:
+ * - Use internal bitcoind wallet, simple utxo manipulation
+ * - Use Parity dev wallet to fund 2 wallets, could be replaced with Ethereum wallet: https://github.com/coblox/bobtimus/issues/78
+ */
 
 import { HelloSwap } from "./helloSwap";
 
